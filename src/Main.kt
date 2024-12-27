628bb698269c9ce7399195e0b19e380aecd47a02
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-12-27 02:47:49 -0600

    Variable types

--- src/Main.kt
+++ src/Main.kt
@@ -10,4 +10,24 @@ fun main() {
     val name : String =  "Mary"
     // name = "Antonium" // this cannot be done, because it is only read variable
     println(PI)
+
+    val booleanVariable : Boolean = true
+    val longNumber : Long = 3L
+    val doubleNumber : Double = 2.87986
+    val floatNumber : Float = 1.1f
+
+    val firstValue = 20
+    val secondValue = 12
+    val thirdValue = firstValue.minus(secondValue)
+    println(thirdValue)
+
+    println(firstValue)
+    println(secondValue)
+
+    val lastName : String = "Meixueiro"
+    val namePerson : String = "Jonathan"
+    val completeName : String = "My full name is: $namePerson $lastName"
+
+    println(completeName)
+
 }
