7d8ef47006f8f1ea067d032fc9b54adecb607e27
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-12-27 02:09:50 -0600

    Variables in kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,13 @@
+// const variables are declares outside the entry point
+const val PI = 3.1416
+
 fun main() {
     println("Hello World!")
+    var money : Int = 10
+    println(money)
+    money = 5
+    println(money)
+    val name : String =  "Mary"
+    // name = "Antonium" // this cannot be done, because it is only read variable
+    println(PI)
 }
