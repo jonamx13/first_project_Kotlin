5e9f954d51abc11cc73fae28b429f9e17c24037c
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-12-27 03:29:15 -0600

    Operators in Kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,32 @@
 // const variables are declares outside the entry point
 const val PI = 3.1416
 
+fun printOperations(firstValue: Int, secondValue: Int) {
+    // Print first and second numbers
+    println("This is the first number: $firstValue")
+    println("This is the second number: $secondValue")
+
+    // List of operations
+    val operationList: List<String> = listOf(
+        "This is an addition operation: a: $firstValue + b: $secondValue c = a + b = ${firstValue + secondValue}",
+        "This is a subtraction operation: a: $firstValue - b: $secondValue c = a - b = ${firstValue - secondValue}",
+        "This is a multiplication operation: a: $firstValue * b: $secondValue c = a * b = ${firstValue * secondValue}",
+        "This is a division operation: a: $firstValue / b: $secondValue c = a / b = ${firstValue / secondValue}",
+        "This is a modulo operation: a: $firstValue % b: $secondValue c = a % b = ${firstValue % secondValue}",
+        "This is a postfix increment operation: a: $firstValue++, b: $secondValue++",
+        "This is a postfix decrement operation: a: $firstValue--, b: $secondValue--",
+        "This is a greater than operation: a: $firstValue > b: $secondValue c = a > b = ${firstValue > secondValue}",
+        "This is a less than operation: a: $firstValue < b: $secondValue c = a < b = ${firstValue < secondValue}",
+        "This is a greater than or equal operation: a: $firstValue >= b: $secondValue c = a >= b = ${firstValue >= secondValue}",
+        "This is a less than or equal operation: a: $firstValue <= b: $secondValue c = a <= b = ${firstValue <= secondValue}",
+        "This is a not equal operation: a: $firstValue != b: $secondValue c = a != b = ${firstValue != secondValue}"
+    )
+
+    for (operation in operationList) {
+        println(operation)
+    }
+}
+
 fun main() {
     println("Hello World!")
     var money : Int = 10
@@ -30,4 +56,10 @@ fun main() {
 
     println(completeName)
 
+    // Operators
+    val firstNumber: Int = 40
+    val secondNumber: Int = 17
+
+    // Call the function to print all operations defined
+    printOperations(firstNumber, secondNumber)
 }
