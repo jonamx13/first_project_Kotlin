250fafc570ee6216b9118bb1f2b76d882ba14a83
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-12-28 03:47:31 -0600

    Control structure: when

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,26 @@
 fun main (args: Array<String>) {
-    val name = "Li"
+    val colorName = "Red"
 
-    if(name.isNotEmpty()) println("The length of the variable \"name\" is ${name.length}") else println("Error, The variable is empty")
-
-    val message : String = if (name.length > 3) {
-        "Your name is long!"
-    } else if (name.isEmpty()) {
-        "Name is empty"
+    when (colorName) {
+        "Yellow" -> println("Yellow is the HAPPINESS color")
+        "Red", "Crimson" -> println("This color symbolizes heat")
+        else -> println("Error, I have no info about color")
     }
-    else {
-        "You have a short name"
+
+    val code : Int = 700
+
+    when(code) {
+        in 200..299 -> println("Everything is going right")
+        in 400..500 -> println("Something has failed")
+        else -> println("Unknown code, something has failed")
+    }
+
+    val shoesSize = 47
+    val message = when(shoesSize) {
+        41,43 -> "We have available shoes"
+        42, 44 -> "We are running out of stock!"
+        45 -> "Sorry, we don't have available stock"
+        else -> "These shoes are only available for sizes: 41, 42, 43 & 45"
     }
     println(message)
 }
