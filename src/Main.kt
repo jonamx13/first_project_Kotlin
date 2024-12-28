b4512524e6f752d087b2102d06fb592b2209caab
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-12-28 03:17:18 -0600

    Control structures: if

--- src/Main.kt
+++ src/Main.kt
@@ -1,65 +1,15 @@
-// const variables are declares outside the entry point
-const val PI = 3.1416
+fun main (args: Array<String>) {
+    val name = "Li"
 
-fun printOperations(firstValue: Int, secondValue: Int) {
-    // Print first and second numbers
-    println("This is the first number: $firstValue")
-    println("This is the second number: $secondValue")
+    if(name.isNotEmpty()) println("The length of the variable \"name\" is ${name.length}") else println("Error, The variable is empty")
 
-    // List of operations
-    val operationList: List<String> = listOf(
-        "This is an addition operation: a: $firstValue + b: $secondValue c = a + b = ${firstValue + secondValue}",
-        "This is a subtraction operation: a: $firstValue - b: $secondValue c = a - b = ${firstValue - secondValue}",
-        "This is a multiplication operation: a: $firstValue * b: $secondValue c = a * b = ${firstValue * secondValue}",
-        "This is a division operation: a: $firstValue / b: $secondValue c = a / b = ${firstValue / secondValue}",
-        "This is a modulo operation: a: $firstValue % b: $secondValue c = a % b = ${firstValue % secondValue}",
-        "This is a postfix increment operation: a: $firstValue++, b: $secondValue++",
-        "This is a postfix decrement operation: a: $firstValue--, b: $secondValue--",
-        "This is a greater than operation: a: $firstValue > b: $secondValue c = a > b = ${firstValue > secondValue}",
-        "This is a less than operation: a: $firstValue < b: $secondValue c = a < b = ${firstValue < secondValue}",
-        "This is a greater than or equal operation: a: $firstValue >= b: $secondValue c = a >= b = ${firstValue >= secondValue}",
-        "This is a less than or equal operation: a: $firstValue <= b: $secondValue c = a <= b = ${firstValue <= secondValue}",
-        "This is a not equal operation: a: $firstValue != b: $secondValue c = a != b = ${firstValue != secondValue}"
-    )
-
-    for (operation in operationList) {
-        println(operation)
+    val message : String = if (name.length > 3) {
+        "Your name is long!"
+    } else if (name.isEmpty()) {
+        "Name is empty"
     }
-}
-
-fun main() {
-    println("Hello World!")
-    var money : Int = 10
-    println(money)
-    money = 5
-    println(money)
-    val name : String =  "Mary"
-    // name = "Antonium" // this cannot be done, because it is only read variable
-    println(PI)
-
-    val booleanVariable : Boolean = true
-    val longNumber : Long = 3L
-    val doubleNumber : Double = 2.87986
-    val floatNumber : Float = 1.1f
-
-    val firstValue = 20
-    val secondValue = 12
-    val thirdValue = firstValue.minus(secondValue)
-    println(thirdValue)
-
-    println(firstValue)
-    println(secondValue)
-
-    val lastName : String = "Meixueiro"
-    val namePerson : String = "Jonathan"
-    val completeName : String = "My full name is: $namePerson $lastName"
-
-    println(completeName)
-
-    // Operators
-    val firstNumber: Int = 40
-    val secondNumber: Int = 17
-
-    // Call the function to print all operations defined
-    printOperations(firstNumber, secondNumber)
+    else {
+        "You have a short name"
+    }
+    println(message)
 }
