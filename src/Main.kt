4679dc18c01e4b79a8fd817dc99140fbaaa2c8a5
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-12-29 05:08:08 -0600

    Elvis operator

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,5 @@
 fun main (args: Array<String>) {
     var name : String? = null
-
-    try {
-        throw NullPointerException("Null reference")
-    } catch (exception : NullPointerException) {
-        println("An error has happened")
-    } finally {
-        println("Finally, an error has happened... closing app")
-    }
-
-    val firstValue = 10
-    val secondValue = 0
-    val result : Int = try { firstValue / secondValue } catch (exception : Exception) { 0 }
-    println(result)
+    var charName : Int = name?.length ?: 0
+    println(charName)
 }
