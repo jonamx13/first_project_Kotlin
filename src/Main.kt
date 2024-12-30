5b243b123d54e263bd4475bb11e7286108d2e0ad
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-12-30 00:51:27 -0600

    Lists

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,34 @@
 fun main (args: Array<String>) {
-    var name : String? = null
-    var charName : Int = name?.length ?: 0
-    println(charName)
+    //immutable
+    val namesList = listOf("Jhon", "Kike", "Camil")
+    println(namesList)
+
+    //mutable
+    val emptyList = mutableListOf<String>()
+    println(emptyList)
+
+    emptyList.add("Jhonny")
+    println(emptyList)
+
+    val valueUsingGet = emptyList.get(0)
+    println(valueUsingGet)
+
+    val valueUsingOperator = emptyList[0]
+    println(valueUsingOperator)
+
+    val firstValue: String? = namesList.firstOrNull()
+    println(firstValue)
+
+    emptyList.removeAt(0)
+    println(emptyList)
+
+    emptyList.add("Charles")
+    println(emptyList)
+
+    emptyList.removeIf { chars -> chars.length > 3 }
+    println(emptyList)
+
+    val myArray = arrayOf(1,2,3,4)
+    println("Our array $myArray")
+    println("Array as list ${myArray.toList()}")
 }
