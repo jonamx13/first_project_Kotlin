15da793f09b735294b6b72f487afefcfc7c74d7a
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2025-01-07 09:59:06 -0600

    "Apply" function to convert nullable to non-nullable

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,12 @@
 fun main (args: Array<String>) {
     val mobileList = mutableListOf("Google Pixel 2Xl", "Google Pixel 4a", "Huawei Redmi 9", "Xiaomi mi a3")
-        .run {
-            removeIf{ mobile -> mobile.contains("Google")}
-            this
-        }
+        .apply { removeIf { mobile -> mobile.contains("Google")} }
 
     println(mobileList)
 
-
+    val colors: MutableList<String>? = mutableListOf("Blue","Yellow","Red")
+    colors?.apply {
+        println("Our colors are: $this")
+        println("The quantity of colors is: $size")
+    }
 }
